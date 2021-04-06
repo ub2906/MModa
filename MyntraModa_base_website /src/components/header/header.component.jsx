9068d6f9bcbd42d73bc58f2d30d295284608bd24
Urja <urja@Urjas-MacBook-Air.local>
2021-04-06 21:32:33 +0530

    renamed

---
+++ MyntraModa_base_website /src/components/header/header.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/mmlogo.svg";
+
+import "./header.styles.scss";
+
+
+const Header = () => ( <
+    div className = "header" >
+    <
+    Link to = "/" >
+    <
+    Logo className = "logo" / >
+    <
+    /Link> <
+    div className = "options" >
+    <
+    Link className = "option"
+    to = "/shop" >
+    SHOP <
+    /Link> <
+    Link className = "option"
+    to = "/tagboard" >
+    TAGBOARD <
+    /Link> <
+    Link className = "option"
+    to = "/signIn" >
+    SIGN IN <
+    /Link> <
+    /div> <
+    /div>
+)
+
+export default Header;
