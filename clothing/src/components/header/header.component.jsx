0b3f4a088d1fc1bfc67a0bff67cc6e64545f89af
Urja Banati <48362988+ub2906@users.noreply.github.com>
2021-04-08 18:58:38 +0530

    party integration

--- clothing/src/components/header/header.component.jsx
+++ clothing/src/components/header/header.component.jsx
@@ -6,24 +6,19 @@ import { ReactComponent as Logo } from "../../assets/mmlogo.svg";
 import "./header.styles.scss";
 
 
-const Header = () => ( <
-    div className = "header" >
-    <
-    Link to = "/" >
-    <
-    Logo className = "logo" / >
-    <
-    /Link>   <
-    div className = "options" >
-    <
-    Link className = "option"
-    to = "/shop" > SHOP < /Link>    <
-    a href = "https://miro.com/app/dashboard/" > TAGBOARD < /a>  &emsp;  <
-    a href = "https://virtualmall.netlify.app/" > VIRTUAL MODA < /a>    <
-    Link className = "option"
-    to = "/signIn" > SIGN IN FOR MODA PARTY < /Link>  <
-    /div>  <
-    /div>
+const Header = () => ( 
+    <div className = "header" >
+        <Link to = "/">
+    <Logo className = "logo" />
+    </Link>    
+    <div className = "options" >
+        <Link className = "option" to = "/shop" > SHOP </Link>     
+        <a href = "https://miro.com/app/dashboard/" > TAGBOARD </a>  &emsp;   
+        <a href = "https://virtualmall.netlify.app/" > VIRTUAL MODA </a> &emsp;  
+        <a href = "https://moda-p.herokuapp.com/#narrow-visitor-check" > MODA PARTY </a>     
+        <Link className = "option" to = "/signIn" > SIGN IN </Link>   
+    </div> 
+    </div>
 )
 
 export default Header;
